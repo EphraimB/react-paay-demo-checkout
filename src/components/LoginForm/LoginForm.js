37c4417b1fb6bf2af460ddfc1f662b2c4f97c1bd
Ephraim Becker <emb@paay.co>
2022-11-07 16:15:34 -0500

    Added the two form fields.

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -5,67 +5,102 @@ import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function LoginForm() {
     function TabPanel(props) {
         const { children, value, index, ...other } = props;
-      
+
         return (
-          <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-          >
-            {value === index && (
-              <Box sx={{ p: 3 }}>
-                <Typography>{children}</Typography>
-              </Box>
-            )}
-          </div>
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+                {...other}
+            >
+                {value === index && (
+                    <Box sx={{ p: 3 }}>
+                        <Typography>{children}</Typography>
+                    </Box>
+                )}
+            </div>
         );
-      }
-      
-      TabPanel.propTypes = {
+    }
+
+    TabPanel.propTypes = {
         children: PropTypes.node,
         index: PropTypes.number.isRequired,
         value: PropTypes.number.isRequired,
-      };
-      
-      function a11yProps(index) {
+    };
+
+    function a11yProps(index) {
         return {
-          id: `simple-tab-${index}`,
-          'aria-controls': `simple-tabpanel-${index}`,
+            id: `simple-tab-${index}`,
+            'aria-controls': `simple-tabpanel-${index}`,
         };
-      }
-        const [value, setValue] = React.useState(0);
-    
-        const handleChange = (event, newValue) => {
-            setValue(newValue);
-        };
-    
-        return (
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'flex-end'
-                }}
-            >
-                <Paper>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                            <Tab label="Sign in" {...a11yProps(0)} />
-                            <Tab label="Register" {...a11yProps(1)} />
-                        </Tabs>
-                    </Box>
-                    <TabPanel value={value} index={0}>
-                        Item One
-                    </TabPanel>
-                    <TabPanel value={value} index={1}>
-                        Item Two
-                    </TabPanel>
-                </Paper>
-            </Box>
-        );
     }
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'flex-end'
+            }}
+        >
+            <Paper>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label="Sign in" {...a11yProps(0)} />
+                        <Tab label="Register" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+                    <Box
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { m: 1, width: '25ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <div>
+                            <TextField id="username" label="Username" variant="standard" />
+                        </div>
+                        <div>
+                            <TextField id="password" type="password" label="Password" variant="standard" />
+                        </div>
+                        <Button type="submit" variant="contained">Log in</Button>
+                    </Box>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                <Box
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { m: 1, width: '25ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <div>
+                            <TextField id="username" label="Username" variant="standard" />
+                        </div>
+                        <div>
+                            <TextField id="password" type="password" label="Password" variant="standard" />
+                        </div>
+                        <div>
+                            <TextField id="confirmPassword" type="password" label="Confirm password" variant="standard" />
+                        </div>
+                        <Button type="submit" variant="contained">Register</Button>
+                    </Box>
+                </TabPanel>
+            </Paper>
+        </Box>
+    );
+}
