cb700b607ee48e74c39db3cc5bdad72e701841ac
Ephraim Becker <emb16@outlook.com>
2022-11-08 11:51:13 -0500

    Added react router.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom"
 import AppBar from './components/AppBar/AppBar';
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <AppBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
