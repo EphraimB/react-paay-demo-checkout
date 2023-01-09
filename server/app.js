956a4d05c2d441991570d2efb021d63e02e0f89b
Ephraim Becker <emb@paay.co>
2023-01-09 16:29:10 -0500

    Added new package and db.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const { Pool } = require('pg')
 const app = express()
 const port = 5000
+
+const pool = new Pool({
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+  })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
