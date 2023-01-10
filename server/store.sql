93c5a16e5b2b5102ab4166df1f92f44d241e73c5
Ephraim Becker <emb16@outlook.com>
2023-01-10 11:10:48 -0500

    Added sql for making the tables.

--- server/store.sql
+++ server/store.sql
@@ -4,3 +4,16 @@ CREATE TABLE products (
     product_description varchar(2048) NOT NULL,
     product_price money
 );
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username varchar(64) NOT NULL,
+    password varchar(512) NOT NULL,
+    is_admin integer NOT NULL
+);
+
+CREATE TABLE cart (
+    cart_id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(user_id),
+    product_id integer NOT NULL REFERENCES products(product_id)
+);
