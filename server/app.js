6fa57b6950ec2a7fed6119eeb6442914f128ae4d
Ephraim Becker <emb16@outlook.com>
2023-01-10 10:27:10 -0500

    Added route to get all products.

--- server/app.js
+++ server/app.js
@@ -1,20 +1,42 @@
-const express = require('express')
-const { Pool } = require('pg')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const crypto = require('crypto');
+const { Pool, Client } = require('pg');
+const dotenv = require('dotenv');
+const port = 5000;
+
+dotenv.config({ override: true });
+
+app.use(cors());
+app.use(express.json());
 
 const pool = new Pool({
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
-  })
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+
+console.log(process.env.PASSWORD);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+app.get("/products", async (req, res) => {
+  try {
+      const allProducts = await pool.query("SELECT * FROM products");
+
+      res.json(allProducts.rows);
+  } catch (err) {
+      console.error(err.message);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`PAAY demo checkout app listening on port ${port}`)
+});
