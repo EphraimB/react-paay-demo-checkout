ecb4305b049d1f8e3a5afde6bd09935bc82475b0
Ephraim Becker <emb@paay.co>
2023-01-11 11:49:07 -0500

    Updated the create tables sql.

--- server/store.sql
+++ server/store.sql
@@ -2,7 +2,7 @@ CREATE TABLE products (
     product_id SERIAL PRIMARY KEY,
     product_title varchar(128) NOT NULL,
     product_description varchar(2048) NOT NULL,
-    product_price money
+    product_price money NOT NULL
 );
 
 CREATE TABLE users (
