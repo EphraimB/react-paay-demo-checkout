809492a1395b241f1c0ee80bc1636e10031604a6
Ephraim Becker <emb16@outlook.com>
2023-01-17 13:24:00 -0500

    Now logs out.

--- client/src/components/LogoutForm/LogoutForm.js
+++ client/src/components/LogoutForm/LogoutForm.js
@@ -1,8 +1,26 @@
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from '@mui/icons-material/Logout';
+import axios from 'axios';
 
 export default function LogoutForm() {
+
+    function handleClick() {
+        axios.post('http://localhost:5000/logout', {
+
+        })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        window.location.reload();
+    }
+
     return (
         <Box
             sx={{
@@ -13,9 +31,16 @@ export default function LogoutForm() {
             }}
         >
             <Paper>
-                <Typography variant="h4" component="h2">
+                <Typography variant="h4" component="h2" paddingBottom={12}>
                     You're logged in
                 </Typography>
+                <Stack
+                    direction="row"
+                    justifyContent="flex-end">
+                    <IconButton aria-label="logout" onClick={handleClick}>
+                        <LogoutIcon />
+                    </IconButton>
+                </Stack>
             </Paper>
         </Box>
     );
