22a342cd8a481726d95be9c6d94d690df3ce6a74
Ephraim Becker <emb16@outlook.com>
2023-01-17 11:48:01 -0500

    Corrected cors config.

--- server/app.js
+++ server/app.js
@@ -11,7 +11,13 @@ const port = 5000;
 
 dotenv.config({ override: true });
 
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const pool = new Pool({
