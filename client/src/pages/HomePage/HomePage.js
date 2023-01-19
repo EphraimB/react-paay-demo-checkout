aebca604502619b1987179aed2aaad59e5062249
Ephraim Becker <emb@paay.co>
2023-01-19 14:00:22 -0500

    Fixed the layout.

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -3,7 +3,6 @@ import Grid from '@mui/material/Unstable_Grid2';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import AppBar from '../../components/AppBar/AppBar';
@@ -14,7 +13,6 @@ import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import { Box } from '@mui/system';
-import Alerts from '../../Alerts/Alerts';
 
 function HomePage({ products, loggedIn, isAdmin }) {
     const [showAddProductForm, setShowAddProductForm] = useState(false);
@@ -29,9 +27,8 @@ function HomePage({ products, loggedIn, isAdmin }) {
 
     const AddProductForm = () => {
         return (
-            <div>
-                <Card sx={{ maxWidth: 512 }} component="form">
-                    {/* {product.image === null ? <ImageIcon /> :
+            <Card sx={{ maxWidth: 512 }} component="form">
+                {/* {product.image === null ? <ImageIcon /> :
                     <CardMedia
                         component="img"
                         height="140"
@@ -39,39 +36,38 @@ function HomePage({ products, loggedIn, isAdmin }) {
                         alt={product.imageDescription}
                     />
                 } */}
-                    <Box sx={{
-                        display: "flex",
-                        justifyContent: "flex-end"
-                    }}>
-                        <IconButton>
-                            <CloseIcon onClick={hideProductForm} />
-                        </IconButton>
-                    </Box>
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            <TextField id="title" label="Title" variant="standard" />
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            <TextField id="description" label="Description" variant="standard" />
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            <TextField id="price" label="Price" type="number" min="1" step="any" variant="standard" />
-                        </Typography>
-                    </CardContent>
-                    <Button type="submit" variant="contained">Add Product</Button>
-                </Card>
-            </div>
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "flex-end"
+                }}>
+                    <IconButton>
+                        <CloseIcon onClick={hideProductForm} />
+                    </IconButton>
+                </Box>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        <TextField id="title" label="Title" variant="standard" />
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        <TextField id="description" label="Description" variant="standard" />
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        <TextField id="price" label="Price" type="number" min="1" step="any" variant="standard" />
+                    </Typography>
+                </CardContent>
+                <Button type="submit" variant="contained">Add Product</Button>
+            </Card>
         )
     }
 
     return (
-        <div>
+        <>
             <AppBar loggedIn={loggedIn} />
-            <Grid container spacing={{ xs: 0, md: 2 }} columns={{ xs: 12, md: 4 }} sx={{ m: 2 }}>
+            <Grid id="products" container spacing={2} columns={{ xs: 12, md: 4 }} sx={{ m: 2 }}>
                 {showAddProductForm ? <AddProductForm /> : null}
                 {Object.values(products).map((product) => {
                     return (
-                        <Card sx={{ maxWidth: 512 }}>
+                        <Card sx={{ maxWidth: 512, m: 2 }}>
                             {/* {product.image === null ? <ImageIcon /> :
                                 <CardMedia
                                     component="img"
@@ -107,7 +103,7 @@ function HomePage({ products, loggedIn, isAdmin }) {
             }}>
                 <AddIcon onClick={showProductForm} />
             </Fab> : null}
-        </div>
+        </>
     );
 }
 
