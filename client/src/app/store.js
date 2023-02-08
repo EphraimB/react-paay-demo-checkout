71558c475ceca3c7f2b58fb411f05f8e6f051d0d
Ephraim Becker <emb@paay.co>
2023-02-08 11:08:23 -0500

    Deleted unneeded auth slice.

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice'
+import { configureStore } from '@reduxjs/toolkit';
 import popupReducer from '../features/Popup/popupSlice';
 import productStateReducer from '../features/productState/productStateSlice';
 import { apiSlice } from '../features/api/apiSlice';
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
         popup: popupReducer,
         productState: productStateReducer,
         [apiSlice.reducerPath]: apiSlice.reducer
