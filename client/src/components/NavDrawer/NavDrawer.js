26ba0fa2498a7426c5d0e00d74311ea393f48479
Ephraim Becker <emb@paay.co>
2023-02-16 11:53:33 -0500

    Added company information and app title to drawer.

--- client/src/components/NavDrawer/NavDrawer.js
+++ client/src/components/NavDrawer/NavDrawer.js
@@ -5,6 +5,9 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import { useNavigate } from "react-router-dom";
+import logo from '../../img/logo.png';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 
 export default function NavDrawer({ openDrawer, handleDrawerToggle }) {
     const navigate = useNavigate()
@@ -16,11 +19,20 @@ export default function NavDrawer({ openDrawer, handleDrawerToggle }) {
             onClose={handleDrawerToggle}
         >
             <Box
-                sx={{ width: 250 }}
+                sx={{ 
+                    width: 250
+                }}
                 role="presentation"
                 onClick={handleDrawerToggle}
                 onKeyDown={handleDrawerToggle}
             >
+                <Box sx={{
+                    m: 2,
+                }}>
+                    <img src={logo} width="100" alt="Logo" />
+                    <Typography variant="subtitle1">PAAY demo checkout</Typography>
+                </Box>
+                <Divider />
                 <List>
                     <ListItem>
                         <ListItemButton onClick={() => navigate('/')}>Home</ListItemButton>
