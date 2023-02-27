6954a5ae5c0df8887afc230228390c99a0a430d5
Ephraim Becker <emb@paay.co>
2023-02-27 16:39:59 -0500

    Switched to MUI elements.

--- client/src/pages/DetailsPage/DetailsPage.js
+++ client/src/pages/DetailsPage/DetailsPage.js
@@ -1,11 +1,24 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import {
     useGetProductQuery
 } from "../../features/api/apiSlice";
 
 export default function DetailsPage() {
+    const {
+        data: products,
+        isLoading,
+        isSuccess,
+        isError,
+        error,
+        refetch
+    } = useGetProductQuery();
+
+
     return (
-        <div>
-            <h1>Product details</h1>
-        </div>
+        <Box>
+            <Typography variant="h6">Product details</Typography>
+        </Box>
     )
 }
