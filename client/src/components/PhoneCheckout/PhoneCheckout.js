346795b7f2bae265d951c0a8014ec5a3f94a1025
Ephraim Becker <emb@paay.co>
2023-02-27 14:33:58 -0500

    Phone number validation.

--- client/src/components/PhoneCheckout/PhoneCheckout.js
+++ client/src/components/PhoneCheckout/PhoneCheckout.js
@@ -1,8 +1,18 @@
+import React, { useState } from 'react';
 import './PhoneCheckout.css';
 import logo from '../../img/logo.png';
 import Button from "@mui/material/Button";
 
 export default function PhoneCheckout() {
+    const [phoneNumber, setPhoneNumber] = useState('');
+
+    const handleChange = (e) => {
+        if (isNaN(e.target.value)) return;
+        if (e.target.value.length > 10) return;
+
+        setPhoneNumber(e.target.value);
+    }
+
     const handleCheckout = () => {
         alert('Phone checkout');
     }
@@ -10,7 +20,7 @@ export default function PhoneCheckout() {
     return (
         <div id="phoneCheckout">
             <img src={logo} alt="logo" height="20" />
-            <input type="text" placeholder="Phone number" />
+            <input type="text" value={phoneNumber} onChange={handleChange} placeholder="Phone number" />
             <Button variant="contained" onClick={handleCheckout}>GO</Button>
         </div>
     )
